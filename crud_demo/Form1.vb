4ef24516691b8536bba66009b4372cba8c6ba44e
Radleyyy <radzzlumabong@gmail.com>
2025-11-25 07:54:11 +0800

    add

--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -1,7 +1,5 @@
 ﻿Imports MySql.Data.MySqlClient
 
-﻿Imports MySql.Data.MySqlClient
-
 Public Class Form1
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
@@ -32,6 +30,23 @@ Public Class Form1
                     MessageBox.Show("Record insert successfully!")
                 End Using
             End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim query As String = "SELECT * FROM crud_demo_db.students_tbl;"
+        Try
+
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database= crud_demo_db")
+                Dim adapter As New MySqlDataAdapter(query, conn) 'get from database'
+                Dim table As New DataTable() 'table object'
+                adapter.Fill(table) 'from adapter to table object
+                DataGridView1.DataSource = table 'display to datagridview
+
+            End Using
+
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
